d319d66085358ab6100e1f6c3a0e788f76c4cd6a
DONEIN.NET <github@donein.net>
2013-03-15 22:07:36 -0700

    Initial commit

---
+++ unblock_files.ps1
@@ -0,0 +1,16 @@
+﻿# === BEGIN EXAMPLE USAGE ==============================================================================
+# 
+# Unblock-Files -path "C:\InetPub\wwwroot"
+#
+# === END EXAMPLE USAGE ================================================================================
+
+
+
+Function Unblock-Files([string]$path = "C:\")
+{ 
+    $folders = dir $path -recurse | Where {$_.PSIsContainer};
+    foreach($folder in $folders)
+    {
+        Get-ChildItem $folder.FullName | Unblock-File -Verbose;
+    }
+}
